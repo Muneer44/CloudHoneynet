5d726cdeaafef4b371c63e952dd51e0c6bfdc612
Mohammed Muneer <117259069+Muneer44@users.noreply.github.com>
2023-08-19 07:21:24 -0400

    Add files via upload

---
+++ Powershell Scripts/AAD_Brute_Force_Simulator.ps1
@@ -0,0 +1,37 @@
+﻿# AAD-BruteForce-Simulator
+# This script fails to login '$max_attempts' times, and then successfully logs in once
+
+# Authenticate user against Azure AD
+$tenantId = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" # Azure Tenant ID
+$username = "User@mmunr44gmail.onmicrosoft.com" # Legit user in AAD
+$correct_password = "Cyberlab123!" # Enter the correct password for the above user
+$wrong_password = "BOGUS PASSWORD" # This is used to generate failure attempts
+$max_attempts = 11 # This is the number of times to fail the login before successful authentication
+
+# Disconnect from AAD if already connected; we want to try to authenticate
+if ((Get-AzContext) -eq $true) {
+    Disconnect-AzAccount
+}
+
+# This section will fail '$max_attempts' logon attempts against Azure AD user
+$count = 0
+
+while ($count -le $max_attempts) {
+    Start-Sleep -Seconds 1
+    $count++
+    try {
+        $securePassword = ConvertTo-SecureString $wrong_password -AsPlainText -Force
+        $cred = New-Object System.Management.Automation.PSCredential ($username, $securePassword)
+        Connect-AzAccount -TenantId $tenantId -Credential $cred -ErrorAction SilentlyContinue
+        Write-Host "Login attempt: ($($count))"
+    }
+    catch {
+        Write-Host "Login Failure. ($($count))"
+        $Error[0].Exception.Message
+    }
+}
+
+# This section will successfully authenticate against AAD, simulating a successful brute force attack
+$securePassword = ConvertTo-SecureString $correct_password -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential ($username, $securePassword)
+Connect-AzAccount -TenantId $tenantId -Credential $cred -ErrorAction SilentlyContinue
---
+++ Powershell Scripts/SQL-Brute-Force-Simulator.ps1
@@ -0,0 +1,44 @@
+﻿﻿$serverName = "44.000.44.000" # SQL Server address
+$databaseName = "master" 
+$username = "Bogus-User" # username to attempt a login with.
+$password = "bad_password_to_generate_auth_failures__"
+$max_attempts = 30 # Number of attempts
+
+
+# Build the connection string using Windows authentication.
+$connectionString = "Server=$serverName;Database=$databaseName;Integrated Security=False;User Id=$username;Password=$password;"
+
+$count = 0
+
+while ($count -lt $max_attempts){
+    $count++
+    try {
+        # Pause the script for 2 seconds to allow for processing
+        Start-Sleep -Seconds 3
+
+        # Open the connection
+        $connection = New-Object System.Data.SqlClient.SqlConnection($connectionString)
+        $connection.Open()
+    
+        # Define the SQL query to execute
+        $query = "SELECT * FROM spt_monitor"
+    
+        # Create a command object and execute the query
+        $command = New-Object System.Data.SqlClient.SqlCommand($query, $connection)
+        $result = $command.ExecuteReader()
+    
+        # Process the query results
+        while ($result.Read()) {
+            Write-Host $result
+        }
+    
+    } catch {
+        # Handle any errors that occur
+        Write-Host "Error: $($Error[0].Exception.Message)"
+    } finally {
+        # Close the connection
+        if ($connection.State -eq "Open") {
+            $connection.Close()
+        }
+    }
+}
